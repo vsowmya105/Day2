096e702917946490b7654d71079fe53299eaf15f
vsowmya105 <99954998+vsowmya105@users.noreply.github.com>
2023-12-20 16:13:14 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
+import java.util.*;
 class Hello{
     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
         System.out.println("Hello world");
+        System.out.println("I am Sowmya --- "+2*x);
     }
 }
